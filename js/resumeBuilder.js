eeaa82ff16bfa2e4c2ac581e84739aef3c550d19
Mike <mike@none.com>
2017-06-16 16:22:22 -0500

    Inital object templating

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,44 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "",
+    "role": "",
+    "welcomeMessage": "",
+    "biopic": "url",
+    "contacts": {
+        "mobile": "",
+        "email": "",
+        "github": "",
+        "twitter": "",
+        "location": ""
+    },
+    "skills": [""],
+    display: function () {
+
+    }
+};
+
+var education = {
+    "schools": [
+        { "name": "", "location": "", "degree": "", "dates": "", "url": "", "majors": [""] }
+    ],
+    "onlineCourses": [
+        { "title": "", "school": "", "dates": "", "url": "" }
+    ]
+};
+
+var work = {
+    "jobs": [
+        { "employer": "", "title": "", "location": "", "dates": "", "description": "" }
+    ],
+    display: function () {
+
+    }
+};
+
+var projects = {
+    projects: [
+        { "title": "", "dates": "", "description": "", "images": [""] }
+    ],
+    display: function () {
+
+    }
+};
